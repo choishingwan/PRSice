687fb891595b40e188e1d2193a248338897ede8c
sam <choishingwan@gmail.com>
2017-05-29 01:23:40 +0100

    Remove unused function

--- inc/misc.hpp
+++ inc/misc.hpp
@@ -128,19 +128,5 @@ inline void replace_substring( std::string &s, const std::string &search, const 
     }
 }
 
-// Again, from stackoverflow https://stackoverflow.com/a/12399290
-template <typename T>
-inline std::vector<size_t> sort_indexes(const std::vector<T> &v) {
-
-    // initialize original index locations
-    std::vector<size_t> idx(v.size());
-    iota(idx.begin(), idx.end(), 0);
-
-    // sort indexes based on comparing values in v
-    sort(idx.begin(), idx.end(),
-            [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
-
-    return idx;
-}
 }
 #endif /* misc_hpp */
