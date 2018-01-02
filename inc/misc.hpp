f13bda3240c78ef6d11bf2912b73134b16fa8913
Choi Shing Wan <choishingwan@gmail.com>
2018-01-02 16:40:02 +0800

    Add one pass SD calculation

--- inc/misc.hpp
+++ inc/misc.hpp
@@ -17,6 +17,7 @@
 #ifndef misc_hpp
 #define misc_hpp
 
+#include <assert.h>
 #include <stdexcept>
 #include <stdio.h>
 #define _USE_MATH_DEFINES
@@ -101,36 +102,37 @@ class RunningStat
 {
 public:
     RunningStat() {}
-    void clear() {
-    		n = 0;
-    		M1=M2=M3=M4=0.0;
+    void clear()
+    {
+        n = 0;
+        M1 = M2 = M3 = M4 = 0.0;
     }
     void push(double x)
     {
-    		double delta, delta_n, delta_n2, term1;
+        double delta, delta_n, delta_n2, term1;
 
-    	    size_t n1 = n;
-    	    n++;
-    	    delta = x - M1;
-    	    assert(n > 0);
-    	    delta_n = delta / n;
-    	    delta_n2 = delta_n * delta_n;
-    	    term1 = delta * delta_n * n1;
-    	    M1 += delta_n;
-    	    M4 += term1 * delta_n2 * (n*n - 3*n + 3) + 6 * delta_n2 * M2 - 4 * delta_n * M3;
-    	    M3 += term1 * delta_n * (n - 2) - 3 * delta_n * M2;
-    	    M2 += term1;
+        size_t n1 = n;
+        n++;
+        delta = x - M1;
+        assert(n > 0);
+        delta_n = delta / n;
+        delta_n2 = delta_n * delta_n;
+        term1 = delta * delta_n * n1;
+        M1 += delta_n;
+        M4 += term1 * delta_n2 * (n * n - 3 * n + 3) + 6 * delta_n2 * M2
+              - 4 * delta_n * M3;
+        M3 += term1 * delta_n * (n - 2) - 3 * delta_n * M2;
+        M2 += term1;
     }
-    size_t n() const { return n; }
+    size_t get_n() const { return n; }
 
-    double mean() const { return M1 }
+    double mean() const { return M1; }
 
-    double var() const { return M2/((double)n-1.0); }
+    double var() const { return M2 / ((double) n - 1.0); }
 
     double sd() const { return sqrt(var()); }
 
 private:
-
     size_t n;
     double M1, M2, M3, M4;
 };
