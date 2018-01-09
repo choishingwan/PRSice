401de85dc2d7c2bc14f13e03c3e2941f4e06494f
Choi Shing Wan <choishingwan@gmail.com>
2018-01-09 14:56:24 +0000

    Still glibc

--- src/genotype.cpp
+++ src/genotype.cpp
@@ -1259,6 +1259,7 @@ void Genotype::efficient_clumping(Genotype& reference, Reporter& reporter)
         { unique_threshold.insert(cur_snp.category()); } }
     fprintf(stderr, "\rClumping Progress: %03.2f%%\n\n", 100.0);
     delete window_data;
+    window_data=nullptr;
     window_data_ptr = nullptr;
     bigstack_ua = nullptr;
     bigstack_initial_base = nullptr;
