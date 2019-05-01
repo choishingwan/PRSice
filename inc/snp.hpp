53ecdd1b5423a5fd23dddf4601aac7984295ac57
Choi Shing Wan <choishingwan@gmail.com>
2019-05-01 14:08:27 -0400

    [Update] Basic SNP testing completed

--- inc/snp.hpp
+++ inc/snp.hpp
@@ -210,7 +210,8 @@ public:
     std::string rs() const { return m_rs; }
     std::string ref() const { return m_ref; }
     std::string alt() const { return m_alt; }
-    bool is_flipped() { return m_flipped; }
+    bool is_flipped() const { return m_flipped; }
+    bool is_ref_flipped() const {return m_ref_flipped; }
 
     /*!
      * \brief check if this SNP is within the i th region
