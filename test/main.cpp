b4502c4dc441452d5df2580b7c5969b69589dd64
Choi Shing Wan <choishingwan@gmail.com>
2019-05-01 12:46:37 -0400

    [Test] Disable assert to check if coverall works

--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,8 @@ std::string path;
 #include <iostream>
 int main(int argc, char* argv[])
 {
-    assert(argc >= 2);
-    path = argv[1];
+    //assert(argc >= 2);
+    //path = argv[1];
     // just in case
     path.append("/");
     ::testing::InitGoogleTest(&argc, argv);
