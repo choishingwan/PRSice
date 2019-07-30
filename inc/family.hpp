bc04af63f0bc12936d5b7abad1efceca1514930d
Choi Shing Wan <choishingwan@gmail.com>
2019-07-30 15:17:39 -0400

    [Update] Add Gaussian Familly

--- inc/family.hpp
+++ inc/family.hpp
@@ -1,11 +1,29 @@
 #ifndef FAMILY_HPP
 #define FAMILY_HPP
 #include <Eigen/Dense>
+#include <math.h>
 class Family
 {
 public:
     Family() {}
     virtual ~Family() {}
+    virtual Eigen::VectorXd variance(const Eigen::VectorXd& /*mu*/) const {}
+    virtual Eigen::VectorXd mu_eta(const Eigen::VectorXd& /*eta*/) const {}
+    virtual Eigen::VectorXd linkinv(const Eigen::VectorXd& /*eta*/) const {}
+    virtual Eigen::VectorXd initialize(const Eigen::VectorXd& /*y*/,
+                                       const Eigen::VectorXd /*weights*/) const
+    {
+    }
+    virtual Eigen::VectorXd link(const Eigen::VectorXd& /*mu_start*/) const {}
+
+    virtual double dev_resids_sum(const Eigen::VectorXd& /*y*/,
+                                  const Eigen::VectorXd& /*mu*/,
+                                  const Eigen::VectorXd& /*wt*/) const
+    {
+    }
+    virtual double var_res() { return 1; }
+    virtual bool valideta(const Eigen::VectorXd& /*eta*/) const { return true; }
+    virtual bool validmu(const Eigen::VectorXd& /*mu*/) const { return true; }
 
 protected:
     double y_log_y(const double y, const double mu) const
@@ -19,9 +37,9 @@ class Binomial : public Family
 public:
     Binomial() {}
     virtual ~Binomial() {}
-    Eigen::VectorXd variance(const Eigen::VectorXd& eta) const
+    Eigen::VectorXd variance(const Eigen::VectorXd& mu) const
     {
-        return eta.array() * (1 - eta.array());
+        return mu.array() * (1 - mu.array());
     }
     Eigen::VectorXd mu_eta(const Eigen::VectorXd& eta) const
     {
@@ -118,9 +136,98 @@ public:
     bool valideta(const Eigen::VectorXd& /*eta*/) const { return true; }
     bool validmu(const Eigen::VectorXd& mu) const
     {
-        return (mu.array().isFinite() == 1).all()
+        return (mu.array().isFinite()).all()
                && (mu.array() > 0.0 && mu.array() < 1.0).all();
     }
 };
 
+class Gaussian : public Family
+{
+public:
+    Gaussian(int link = 0) : m_link(link) {}
+    virtual ~Gaussian() {}
+    Eigen::VectorXd variance(const Eigen::VectorXd& mu) const
+    {
+        return Eigen::VectorXd::Constant(mu.rows(), 1);
+    }
+    Eigen::VectorXd mu_eta(const Eigen::VectorXd& eta) const
+    {
+        switch (m_link)
+        {
+        case 1:
+        {
+            Eigen::VectorXd mueta = eta.array().exp();
+            return (mueta.array() > std::numeric_limits<double>::epsilon())
+                .select(std::numeric_limits<double>::epsilon(), mueta);
+        }
+        case 2: return -1.0 / (eta.array().square());
+        case 0:
+        default: return Eigen::VectorXd::Constant(eta.rows(), 1);
+        }
+    }
+    Eigen::VectorXd linkinv(const Eigen::VectorXd& eta) const
+    {
+        switch (m_link)
+        {
+        case 1:
+        {
+            Eigen::VectorXd tmp = eta.array().exp();
+            return (tmp.array() > std::numeric_limits<double>::epsilon())
+                .select(std::numeric_limits<double>::epsilon(), tmp);
+        }
+        case 2: return 1.0 / eta.array();
+        case 0:
+        default: return eta;
+        }
+    }
+    Eigen::VectorXd initialize(const Eigen::VectorXd& y,
+                               const Eigen::VectorXd /*weights*/) const
+    {
+        switch (m_link)
+        {
+        case 2:
+            if ((y.array() == 0).any())
+            {
+                throw std::runtime_error(
+                    "Error: Cannot find valid starting values!");
+            }
+            break;
+        case 1:
+            if ((y.array() <= 0).any())
+            {
+                throw std::runtime_error(
+                    "Error: Cannot find valid starting values!");
+            }
+            break;
+        }
+        return y;
+    }
+    Eigen::VectorXd link(const Eigen::VectorXd& mu_start) const
+    {
+        switch (m_link)
+        {
+        case 0:
+        default: return mu_start;
+        case 1: return log(mu_start.array());
+        case 2: return 1.0 / mu_start.array();
+        }
+    }
+
+    double dev_resids_sum(const Eigen::VectorXd& y, const Eigen::VectorXd& mu,
+                          const Eigen::VectorXd& wt) const
+    {
+        return (wt.array() * ((y.array() - mu.array()).square())).sum();
+    }
+    double var_res() { return 1; }
+    bool valideta(const Eigen::VectorXd& eta) const
+    {
+        if (m_link == 2)
+        { return (eta.array().isFinite()).all() && (eta.array() != 0).all(); }
+        return true;
+    }
+    bool validmu(const Eigen::VectorXd& /*mu*/) const { return true; }
+
+private:
+    int m_link = 0; // 0 = identity, 1 = log, 2 = inverse
+};
 #endif // FAMILY_HPP
