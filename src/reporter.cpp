103261ba9db085d6b47aad8fb5d7075d3cb1fa08
Choi Shing Wan <choishingwan@gmail.com>
2019-08-06 11:42:47 -0400

    [Update] Now able to remove coloring from log file
    So that we don't have the ugly ascii code when reading the log in text editors

--- src/reporter.cpp
+++ src/reporter.cpp
@@ -22,9 +22,7 @@ void split(std::vector<std::string>& result, const char* str, char c = ' ')
     do
     {
         const char* begin = str;
-
         while (*str != c && *str) str++;
-
         result.push_back(std::string(begin, str));
     } while (0 != *str++);
 }
@@ -36,8 +34,10 @@ void Reporter::report(const std::string& input, bool wrap)
     std::vector<std::string> paragraph;
     std::vector<std::string> line;
     std::vector<std::string> check_list;
-    split(paragraph, input.c_str(), '\n');
+    // split(paragraph, input.c_str(), '\n');
+    paragraph = misc::split(input, "\n");
     std::string list_prefix;
+    std::string decolour;
     size_t space_pad = 0;
     for (auto&& message : paragraph)
     {
@@ -49,24 +49,6 @@ void Reporter::report(const std::string& input, bool wrap)
             !message.compare(0, m_error_prefix_size, m_error_prefix);
         bool is_warning =
             !message.compare(0, m_warning_prefix_size, m_warning_prefix);
-        bool is_list = false;
-        if (!is_error && !is_warning)
-        {
-            // check if there are any list in this paragraph
-            // list shouldn't be included in error or warning messages
-            // list is assumed to be in paragraph format
-            // (no in line list allowed)
-            split(check_list, message.c_str(), ')');
-            if (check_list.size() > 1)
-            {
-                if (misc::isNumeric(check_list.front()))
-                {
-                    is_list = true;
-                    list_prefix = check_list.front() + ")";
-                    space_pad = list_prefix.size() + 1;
-                }
-            }
-        }
         if (is_error)
         {
             std::cerr << m_error_color_start;
@@ -77,10 +59,27 @@ void Reporter::report(const std::string& input, bool wrap)
             std::cerr << m_warning_color_start;
             space_pad = m_warning_prefix_size + 1;
         }
+        bool is_list = false;
+        check_list = misc::split(message, ")");
+        if (check_list.size() > 1)
+        {
+            if (misc::isNumeric(check_list.front()))
+            {
+                is_list = true;
+                list_prefix = check_list.front() + ")";
+                space_pad = list_prefix.size() + 1;
+            }
+        }
+        // check if the message contain colour code
+        // and exclude it in the m_log_file output
         if (!wrap)
         {
             std::cerr << message;
-            m_log_file << message;
+            decolour = message;
+            misc::replace_substring(decolour, m_error_color_start, "");
+            misc::replace_substring(decolour, m_warning_color_start, "");
+            misc::replace_substring(decolour, m_color_end, "");
+            m_log_file << decolour;
         }
         else
         {
@@ -90,10 +89,14 @@ void Reporter::report(const std::string& input, bool wrap)
             size_t cur_length = 0;
             for (auto&& word : line)
             {
-                if (word.length() + cur_length >= m_width)
+                decolour = word;
+                misc::replace_substring(decolour, m_warning_color_start, "");
+                misc::replace_substring(decolour, m_error_color_start, "");
+                misc::replace_substring(decolour, m_color_end, "");
+                if (decolour.length() + cur_length >= m_width)
                 {
                     // word is too long, so display on the next line
-                    cur_length = word.length() + 1 + space_pad;
+                    cur_length = decolour.length() + 1 + space_pad;
                     std::cerr << '\n';
                     m_log_file << '\n';
                     std::cerr << std::string(space_pad, ' ');
@@ -101,9 +104,9 @@ void Reporter::report(const std::string& input, bool wrap)
                 }
                 // line still have space. Add the word directly
                 else
-                    cur_length += word.length() + 1;
+                    cur_length += decolour.length() + 1;
                 std::cerr << word << " ";
-                m_log_file << word << " ";
+                m_log_file << decolour << " ";
             }
         }
         if (is_error || is_warning) std::cerr << m_color_end;
