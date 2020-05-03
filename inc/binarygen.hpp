ff8334f0839e3183d82393cf1fa574805e64380b
Choi Shing Wan <choishingwan@gmail.com>
2020-05-03 21:04:00 +0800

    [Fix] LD should use founder vector, not prs vector

--- inc/binarygen.hpp
+++ inc/binarygen.hpp
@@ -89,14 +89,8 @@ protected:
         std::unordered_set<std::string>& processed_snps,
         std::vector<bool>& retain_snp, bool& chr_error, bool& sex_error,
         Genotype* genotype);
-    /*!
-     * \brief This function will read in the bgen probability data and transform
-     * it into PLINK bianry
-     *
-     * \param genotype is the vector containing the plink binary
-     * \param byte_pos is the streampos of the bgen file, for quick seek
-     * \param file_name is the file name of the bgen file
-     */
+
+
     inline void read_genotype(uintptr_t* genotype,
                               const std::streampos byte_pos,
                               const size_t& file_idx)
@@ -105,78 +99,36 @@ protected:
             (m_unfiltered_sample_ct + 3) / 4;
         if (m_ref_plink)
         {
-            // when m_ref_plink is set, it suggest we are using the
-            // intermediate, which is a binary plink format. Therefore we can
-            // directly read in the binary data to genotype, this should already
-            // be well formated when we write it into the file
-            // intermediate file, so don't need .bgen
-            std::string file_name = m_genotype_file_names[file_idx];
-            m_genotype_file.read(file_name, byte_pos, unfiltered_sample_ct4,
+            m_genotype_file.read(m_genotype_file_names[file_idx], byte_pos,
+                                 unfiltered_sample_ct4,
                                  reinterpret_cast<char*>(genotype));
         }
-        // if not, we will try to parse the binary GEN format into a plink
-        // format (using the default intermediate flag = false)
         else if (load_and_collapse_incl(byte_pos, file_idx, genotype))
         {
             throw std::runtime_error("Error: Cannot read the bgen file!");
         }
     }
 
-    /*!
-     * \brief We modify the load_and_collapse_incl function from PLINK to read
-     * in and reformat the bgen data into the binary PLINK data
-     *
-     * \param byte_pos is the location of the file
-     * \param file_name is the file name
-     * \param unfiltered_sample_ct is the number of unfiltered sample
-     * \param sample_ct is the number of sample we want
-     * \param sample_include is the bianry vector containing the boolean
-     * indicate if we want the target sample
-     *
-     * \param final_mask is a mask to remove trailing bytes
-     * \param do_reverse is implemented in PLINK but we will not use
-     * \param rawbuf is the raw vector to store the information
-     * \param mainbuf is the result vector containing the genotype information
-     * \param intermediate is boolean indicate if we want to generate an
-     * intermediate file
-     * \return 0 if sucessful
-     */
-    uint32_t load_and_collapse_incl(const std::streampos byte_pos,
-                                    const size_t& file_idx,
-                                    uintptr_t* __restrict mainbuf)
+    bool load_and_collapse_incl(const std::streampos byte_pos,
+                                const size_t& file_idx,
+                                uintptr_t* __restrict mainbuf)
     {
-        // check sample size != 0
+
         assert(m_unfiltered_sample_ct);
-        // we first check if we will read in a new file
-        auto&& context = m_context_map[file_idx];
-        // we initailize the PLINK generator with the m_sample_include
-        // vector, the mainbuf (result storage) and also the hard threshold.
-        // We don't need to bother about founder or founder info here as all
-        // samples in bgen are considered as founder (at least this is the
-        // case at the moment)
-        // TODO, might want to make this struct a member so that we don't
-        // need to re-initialize it? Though it only contain pointers and
-        // doesn't have any big structures
-        PLINK_generator setter(&m_calculate_prs, mainbuf, m_hard_threshold,
-                               m_dose_threshold);
-        // we can now use the bgen library to parse the BGEN input and
-        // transform it into PLINK format (NOTE: The
-        // read_and_parse_genotype_data_block function has been modified
-        // such that it will always call .sample_completed() when finish
-        // reading each sample. This allow for a more elegant implementation
-        // on our side
-        std::string file_name = m_genotype_file_names[file_idx] + ".bgen";
-        // WARNING: Problem here
-        genfile::bgen::read_and_parse_genotype_data_block<PLINK_generator>(
-            m_genotype_file, file_name, context, setter, &m_buffer1, &m_buffer2,
-            byte_pos);
-        // output from load_raw should have already copied all samples
-        // to the front without the need of subseting
-        // mainbuf should contains the information
-        // update the m_prev_loc counter. However, for bgen, it is most likely
-        // that we will need to seek for every SNP as there are padded data
-        // between each SNP's genotype
-        return 0;
+        try
+        {
+            PLINK_generator setter(&m_sample_for_ld, mainbuf, m_hard_threshold,
+                                   m_dose_threshold);
+            genfile::bgen::read_and_parse_genotype_data_block<PLINK_generator>(
+                m_genotype_file, m_genotype_file_names[file_idx] + ".bgen",
+                m_context_map[file_idx], setter, &m_buffer1, &m_buffer2,
+                byte_pos);
+        }
+        catch (...)
+        {
+            return false;
+        }
+        return true;
     }
 
     void read_score(std::vector<PRS>& prs_list,
